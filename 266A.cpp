aef43b1ee8e05c4f1bf5d18f7ddc0943fae13939
leo96tush <41864567+leo96tush@users.noreply.github.com>
2024-11-30 19:20:23 +0530

    Update 266A.cpp

--- 266A.cpp
+++ 266A.cpp
@@ -29,3 +29,23 @@ OutputCopy
 
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a, count = 0 ;
+    string s ;
+
+    cin >> a >> s ;
+
+    for( auto i=1 ; i<=a ; i++ ) {
+        if ( s[i] == s[i-1] ) {
+            count++ ;
+        }
+    }
+
+    cout << count << endl ;
+    
+    return 0 ;
+}
